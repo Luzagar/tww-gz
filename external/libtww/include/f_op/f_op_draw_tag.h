a2f3796ced4e1dbacd2310ce5d4097d76e40a577
CGF95 <CGF95@users.noreply.github.com>
2025-03-25 15:55:10 +1100

    Update f_op_draw_tag.h

--- external/libtww/include/f_op/f_op_draw_tag.h
+++ external/libtww/include/f_op/f_op_draw_tag.h
@@ -5,16 +5,15 @@
 #include "../dolphin/mtx/vec.h"
 
 struct MatrixPtr {
-    /* 0x000 */ u8 field_0x0[0x6C];
-    /* 0x06C */ Vec target;
-    /* 0x070 */ f32 field_0x070;
-    /* 0x07C */ Vec pos;
-    /* 0x080 */ f32 field_0x080;
-    /* 0x084 */ f32 zoom;
-    /* 0x088 */ f32 roll;
-    /* 0x08C */ u32 field_0x088;
-    /* 0x16F */ u8 field_0x16B[0xE3];
-    /* 0x170 */ u8 camera_mode;
+    /* 0x000 */ u8 field_0x0[0x74];
+    /* 0x074 */ f32 field_0x074; // Seems to be related to the zoom
+    /* 0x084 */ Vec target;
+    /* 0x094 */ Vec pos;
+    /* 0x098 */ f32 roll;
+    /* 0x09C */ f32 zoom;
+    /* 0x0A0 */ u32 field_0x088;
+    /* 0x183 */ u8 field_0x16B[0xC3];
+    /* 0x184 */ u8 camera_mode;
 };
 
 struct MatrixInfo {
